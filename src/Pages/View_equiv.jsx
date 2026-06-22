36e1208b8de8d57b19c565d1df1146dd47787f50
friedaabdo <friedabdo@gmail.com>
2026-06-22 14:30:13 -0400

    start of view equiv page

---
+++ src/Pages/View_equiv.jsx
@@ -0,0 +1,17 @@
+// generate a page where we can see the equivalent courses that are in the database as a list. i want to post a list of the courses assosicated with the learning experience and then be able to click on them to see the details of the course.
+import "./View_equiv.css";
+// import React, { useState, useEffect } from "react";
+// import { useNavigate, useLocation } from "react-router-dom";
+// import axios from "axios";
+
+function View_equiv() {
+    return (
+        <div>
+            <h1>View Equivalencies</h1>
+            <p>This page will list the CUNY courses that have been marked as equivalent to the learning experience.</p>
+        </div>
+    );
+}
+
+export default View_equiv;
+
