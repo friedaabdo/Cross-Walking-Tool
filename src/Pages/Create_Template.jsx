3bdf222c6a37a9bbbd9851068d5daf7ec89f727a
friedaabdo <friedabdo@gmail.com>
2026-09-08 12:32:02 -0400

    another case issue for netlify

--- src/Pages/Create_Template.jsx
+++ src/Pages/Create_Template.jsx
@@ -1,6 +1,6 @@
 import "./Create_template.css";
 import axios from "axios";
-import InputLine from "../Components/inputline";
+import InputLine from "../Components/inputLine";
 import Textarea from "../Components/textarea";
 import OutcomeCard from "../Components/Outcome-card";
 import { parseInputToOutcomeSections } from "../utils/outcomeText";
